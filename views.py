ae29146ced6ee8cc6efdb76032fe707e023e6a28
Sujit5987 <sujitdash5987@gmail.com>
2024-10-13 18:15:46 +0530

    in views.py we can write the python code

--- views.py
+++ views.py
@@ -1,30 +1,138 @@
-from django.contrib import admin
-from django.urls import path,include
-from employee.views import *
-from django.contrib import admin
-from django.urls import path
-from employee.views import *
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.conf import settings
-from django.conf.urls.static import static
+from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpRequest
+from .models import *
+from django.contrib import messages
+from django.contrib.auth import authenticate
 
-urlpatterns = [
-    #path('admin/', admin.site.urls),
-    path('indexemployee/',indexemployee,name="indexemployee"),
-    path('delete/<int:id>/',delete,name='delete'),
-    path('update/<int:id>/',update,name='update'),
-    path('home/serch/',serch,name="serch"),
-    path('home/',home,name="home"),
-    path('home/home/',home,name="home"),
-    path('home/indexemployee/',indexemployee,name="indexemployee"),
-    path('home/home/add/',add,name="add"),
-    path('signup/',signup,name="signup"),
-    path('login/',login,name="login"),
-    path('login/signup/',signup,name="signup"),
-    path('home/signup/',signup,name="signup"),
-    path('home/login/',login,name="login"),
-]
+def home(request):
+         return render(request,"home.html")
 
-if settings.DEBUG:
-    urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-    urlpatterns +=staticfiles_urlpatterns()
+def add(request):
+    if request.method=="POST":
+        data=request.POST
+        ename=data.get("ename")
+        eage=data.get("eage")
+        eeducation=data.get("eeducation")
+        edateofbirth=data.get("edateofbirth")
+        etype=data.get("etype")
+        eimage=request.FILES.get("eimage")
+        employee.objects.create(
+            ename=ename,
+            eage=eage,
+            eeducation=eeducation,
+            edateofbirth=edateofbirth,
+            etype=etype,
+            eimage=eimage
+        )
+        #return redirect('/indexemployee/',employees)
+        employees=employee.objects.all()
+        return render(request,"indexemployee.html",{"employees":employees})
+    return render(request,"add.html")
+
+     
+
+def indexemployee(request):
+    if request.method=="POST":
+        data=request.POST
+        ename=data.get("ename")
+        eage=data.get("eage")
+        eeducation=data.get("eeducation")
+        edateofbirth=data.get("edateofbirth")
+        etype=data.get("etype")
+        eimage=request.FILES.get("eimage")
+        employee.objects.create(
+            ename=ename,
+            eage=eage,
+            eeducation=eeducation,
+            edateofbirth=edateofbirth,
+            etype=etype,
+            eimage=eimage
+        )
+        #return redirect('/indexemployee/',employees)
+    employees=employee.objects.all()
+    return render(request,"indexemployee.html",{"employees":employees})
+
+def update(request,id):
+    employees=employee.objects.get(id=id) 
+    if request.method=="POST":
+        data=request.POST
+        ename=data.get("ename")
+        eage=data.get("eage")
+        eeducation=data.get("eeducation")
+        edateofbirth=data.get("edateofbirth")
+        etype=data.get("etype")
+        eimage=request.FILES.get("eimage")
+        employees.ename=ename
+        employees.eage=eage
+        employee.eeducation=eeducation
+        employees.edateofbirth=edateofbirth
+        employees.etype=etype
+        if edateofbirth:
+            employees.edateofbirth = edateofbirth
+        if eimage:
+                employees.eimage=eimage
+        employees.save()
+        return redirect("/indexemployee/")
+    c={"employees":employees}
+    return render(request,"update.html",c)
+
+def delete(request,id):
+     employees=employee.objects.get(id=id)
+     employees.delete()
+     return redirect('/indexemployee/')
+
+def serch(request):
+        if request.method=="POST":
+            data=request.POST
+            query=data.get("q")
+            if query:
+                if query=="":
+                    messages.info(request, "Empty")
+                    return redirect("/serch/")
+                employees=employee.objects.filter(ename__icontains=query)
+            else:
+                messages.info(request, "NOT FOUND")
+                return redirect("/serch/")
+            if not employee.objects.filter(ename__icontains=query):
+                messages.info(request, "NOT in database")
+                return redirect("/serch/")
+            return render(request, "serch.html",{"employees":employees})
+        return render(request, "serch.html")
+
+def signup(request):
+        if request.method=="POST":
+            data=request.POST
+            first_name=data.get("first_name")
+            username=data.get("username")
+            password=data.get("password")
+            user=User.objects.filter(username=username)
+            if user.exists():
+                 messages.info(request, "Username already exsist ")
+                 return redirect("/signup/")
+            user=User.objects.create(
+                 username=username,
+                 password=password
+            )
+            user.set_password(password)
+            user.save()
+            messages.info(request,"sign up sucessfully done ")
+            return redirect("/login/")
+        return render(request,"signup.html")
+
+def login(request):
+        if request.method=="POST":
+            data=request.POST
+            username=data.get("username")
+            password=data.get("password")
+            print(username)
+            print(password)
+            user=authenticate(username=username,password=password)
+            if not User.objects.filter(username=username).exists():
+                messages.error(request,"username doesnot exists ! Sign Up")
+                return redirect("/login/")
+            if user is None:
+                messages.error(request,"Invalid Username or password !")
+                return redirect("/login/")
+            return redirect("/home/")  
+        return render(request,"login.html")
